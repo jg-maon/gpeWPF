0f746bdb066320138ca0a7b26a8a21c4722701d8
jg-maon <maomona.0513@gmail.com>
2015-11-24 00:41:51 +0900

    ID詳細

---
+++ WpfApplication1/IdInfoTablePane2ViewModel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// ID詳細タブVM
+    /// </summary>
+    class IdInfoTablePane2ViewModel : DocumentViewModel
+    {
+        IdInfoTablePaneContentViewModel m_content;
+        public IdInfoTablePaneContentViewModel Content
+        {
+            get
+            {
+                return m_content;
+            }
+            set
+            {
+                SetProperty(ref m_content, value);
+            }
+        }
+        public IdInfoTablePane2ViewModel(CategoryTreePaneViewModel file)
+            : base("ID詳細情報")
+        {
+            m_content = new IdInfoTablePaneContentViewModel(file);
+        }
+    }
+
+
+    /// <summary>
+    /// ID詳細情報タブ用中身
+    /// </summary>
+    class IdInfoTablePaneContentViewModel : ViewModelBase
+    {
+        private ObservableCollection<IdInfoTableTabPageViewModel> m_parameterTabPages = new ObservableCollection<IdInfoTableTabPageViewModel>();
+        private ReadOnlyObservableCollection<IdInfoTableTabPageViewModel> m_readonlyParameterTabPages = null;
+        public ReadOnlyObservableCollection<IdInfoTableTabPageViewModel> ParameterTabPages
+        {
+            get
+            {
+                return m_readonlyParameterTabPages = m_readonlyParameterTabPages ?? new ReadOnlyObservableCollection<IdInfoTableTabPageViewModel>(m_parameterTabPages);
+            }
+        }
+
+        /// <summary>
+        /// 対象ファイル
+        /// </summary>
+        private readonly CategoryTreePaneViewModel m_file;
+
+        public IdInfoTablePaneContentViewModel(CategoryTreePaneViewModel file)
+        {
+            m_file = file;
+
+        }
+
+
+
+
+
+        /// <summary>
+        /// パラメータタブを開く
+        /// </summary>
+        /// <param name="categoryViewModel">カテゴリ</param>
+        internal void OpenParameterTab(ParameterCollectionViewModel categoryViewModel)
+        {
+            var openTabPage = new IdInfoTableTabPageViewModel(m_file, categoryViewModel);
+
+            var tabPage = m_parameterTabPages.FirstOrDefault((page => page.Equals(openTabPage)));
+            // 開かれていない場合
+            if (null == tabPage)
+            {
+                m_parameterTabPages.Add(openTabPage);
+                tabPage = openTabPage;
+            }
+            tabPage.IsActive = true;
+            tabPage.IsSelected = true;
+        }
+    }
+}
---
+++ WpfApplication1/IdInfoTableTabPageViewModel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace WpfApplication1
+{
+    class IdInfoTableTabPageViewModel : DocumentViewModel, IEquatable<IdInfoTableTabPageViewModel>
+    {
+        private readonly CategoryTreePaneViewModel m_file;
+        public CategoryTreePaneViewModel File
+        {
+            get
+            {
+                return m_file;
+            }
+        }
+        private readonly ParameterCollectionViewModel m_categoryViewModel;
+
+        public string CategoryName
+        {
+            get
+            {
+                return m_categoryViewModel.Name;
+            }
+        }
+
+
+        public ObservableCollection<ParametersViewModel> Parameters
+        {
+            get
+            {
+                return m_categoryViewModel.Parameters;
+            }
+
+        }
+
+
+        private ParametersViewModel m_selectedItem;
+        public ParametersViewModel SelectedItem
+        {
+            get
+            {
+                return m_selectedItem;
+            }
+            set
+            {
+                SetProperty(ref m_selectedItem, value);
+                if(null != SelectedItem)
+                {
+                    Workspace.Instance.UpdateParameterTab(m_file, SelectedItem, false);
+                }
+            }
+        }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="file">ファイル</param>
+        /// <param name="categoryViewModel">カテゴリ</param>
+        public IdInfoTableTabPageViewModel(CategoryTreePaneViewModel file, ParameterCollectionViewModel categoryViewModel)
+            : base(categoryViewModel.Name)
+        {
+            this.m_file = file;
+            this.m_categoryViewModel = categoryViewModel;
+            
+        }
+
+
+        #region IEquatable<IdInfoTableTabPageViewModel> メンバー
+        /// <summary>
+        /// 比較
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public bool Equals(IdInfoTableTabPageViewModel other)
+        {
+            if(m_file != other.m_file)
+            {
+                return false;
+            }
+            if(m_categoryViewModel != other.m_categoryViewModel)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        #endregion
+    }
+}
