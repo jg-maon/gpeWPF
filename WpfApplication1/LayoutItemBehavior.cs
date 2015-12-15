7bf78c013a3344767738eef8435e40679b8f2799
jg-maon <maomona.0513@gmail.com>
2015-12-16 01:10:11 +0900

    ウィンドウのリサイズでプロパティ側が変更されるように

--- WpfApplication1/LayoutItemBehavior.cs
+++ WpfApplication1/LayoutItemBehavior.cs
@@ -31,6 +31,64 @@ namespace WpfApplication1
             if(null == layoutItem)
             { return; }
             layoutItem.LayoutElement.FloatingWidth = (double)e.NewValue;
+            if (!GetSizeChangeEventAdded(d))
+            {
+                SetLayoutItem(layoutItem.LayoutElement, layoutItem);
+                //layoutItem.SizeChanged += layoutItem_SizeChanged;
+                layoutItem.LayoutElement.PropertyChanged += LayoutElement_PropertyChanged;
+                SetSizeChangeEventAdded(layoutItem, true);
+            }
+        }
+
+
+
+        public static Xceed.Wpf.AvalonDock.Controls.LayoutItem GetLayoutItem(DependencyObject obj)
+        {
+            return (Xceed.Wpf.AvalonDock.Controls.LayoutItem)obj.GetValue(LayoutItemProperty);
+        }
+
+        public static void SetLayoutItem(DependencyObject obj, Xceed.Wpf.AvalonDock.Controls.LayoutItem value)
+        {
+            obj.SetValue(LayoutItemProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for LayoutItem.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LayoutItemProperty =
+            DependencyProperty.RegisterAttached("LayoutItem", typeof(Xceed.Wpf.AvalonDock.Controls.LayoutItem), typeof(LayoutItemBehavior), new PropertyMetadata(null));
+
+
+
+
+
+        private static bool GetSizeChangeEventAdded(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(SizeChangeEventAddedProperty);
+        }
+
+        private static void SetSizeChangeEventAdded(DependencyObject obj, bool value)
+        {
+            obj.SetValue(SizeChangeEventAddedProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for SizeChangeEventAdded.  This enables animation, styling, binding, etc...
+        private static readonly DependencyProperty SizeChangeEventAddedProperty =
+            DependencyProperty.RegisterAttached("SizeChangeEventAdded", typeof(bool), typeof(LayoutItemBehavior), new PropertyMetadata(false));
+
+
+
+        static void LayoutElement_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            var layout = sender as Xceed.Wpf.AvalonDock.Layout.LayoutContent;
+            var layoutElement = sender as Xceed.Wpf.AvalonDock.Layout.LayoutElement;
+            var layoutItem = GetLayoutItem(layoutElement);
+            if(e.PropertyName == "FloatingHeight")
+            {
+                SetFloatingHeight(layoutItem, layout.FloatingHeight);
+            }
+            else if(e.PropertyName == "FloatingWidth")
+            {
+                SetFloatingWidth(layoutItem, layout.FloatingWidth);
+            }
         }
 
 
@@ -55,6 +113,13 @@ namespace WpfApplication1
             if (null == layoutItem)
             { return; }
             layoutItem.LayoutElement.FloatingHeight = (double)e.NewValue;
+            if(!GetSizeChangeEventAdded(d))
+            {
+                //layoutItem.SizeChanged += layoutItem_SizeChanged;
+                SetLayoutItem(layoutItem.LayoutElement, layoutItem);
+                layoutItem.LayoutElement.PropertyChanged += LayoutElement_PropertyChanged;
+                SetSizeChangeEventAdded(d, true);
+            }
 
         }
 
