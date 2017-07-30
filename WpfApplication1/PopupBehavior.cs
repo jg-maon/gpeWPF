5f19979835fa6d24b0fd8c0dbb95cf4cf7c98569
jg-maon <maomona.0513@gmail.com>
2017-07-31 00:39:24 +0900

    ポップアップテスト

---
+++ WpfApplication1/PopupBehavior.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace WpfApplication1
+{
+    class PopupBehavior : Behavior<Popup>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            //AssociatedObject.PlacementTarget.MouseEnter += _MouseEntered;
+            //AssociatedObject.PlacementTarget.MouseLeave += _MouseLeaved;
+            AssociatedObject.PlacementTarget.MouseMove += _MouseMoved;
+            //Mouse.AddPreviewMouseMoveHandler(AssociatedObject.PlacementTarget, _MouseMoved);
+
+        }
+
+        private void _MouseLeaved(object sender, MouseEventArgs e)
+        {
+            AssociatedObject.IsOpen = false;
+        }
+
+        private void _MouseEntered(object sender, MouseEventArgs e)
+        {
+            AssociatedObject.IsOpen = true;
+        }
+
+        private void _MouseMoved(object sender, MouseEventArgs e)
+        {
+            var current = e.GetPosition(AssociatedObject.PlacementTarget);
+            AssociatedObject.HorizontalOffset = current.X;
+            AssociatedObject.VerticalOffset = current.Y;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            if (null != AssociatedObject)
+            {
+                AssociatedObject.PlacementTarget.MouseMove -= _MouseMoved;
+                AssociatedObject.PlacementTarget.MouseEnter -= _MouseEntered;
+                AssociatedObject.PlacementTarget.MouseLeave -= _MouseLeaved;
+            }
+            //Mouse.RemovePreviewMouseMoveHandler(AssociatedObject.PlacementTarget, _MouseMoved);
+        }
+    }
+}
